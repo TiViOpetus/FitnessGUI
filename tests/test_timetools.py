8c7c5d25630d589aca5a55c85b43ae24500d6bd2
Mika Vainio <koti.mika.vainio@gmail.com>
2023-04-28 11:13:22 +0300

    Lisätty tests-kansio ja siihen testi

---
+++ tests/test_timetools.py
@@ -0,0 +1,16 @@
+# TESTS FOR MODULE TIMETOOLS.PY
+
+# Lets import module to be tested
+import timetools
+
+# UNIT TESTS DEFINITIONS
+
+# Test if datediff function calculates correct and absolute values
+def test_datediff():
+    assert timetools.datediff('2023-04-28', '2023-04-10') == 18
+    assert timetools.datediff('2023-04-10', '2023-04-28') == 18
+    
+# Test if timediff function calculates correct and absolute values
+def test_timediff():
+    assert round(timetools.timediff('11:30:15', '10:10:05'), 4) == 1.3361
+    assert round(timetools.timediff('10:10:05', '11:30:15'), 4) == 1.3361
